bc60f7e4622dc598ac3f9f79d51a78eedbe14347
David Guimaraes <david.ce@live.com>
2020-04-10 10:13:35 -0400

    05. Adding an alternative implementation of the Insertion algorithm.

--- src/main/java/co/davidguimaraes/sort/InsertionSort.java
+++ src/main/java/co/davidguimaraes/sort/InsertionSort.java
@@ -28,4 +28,27 @@ public class InsertionSort {
             }
         }
     }
+
+    /**
+     * Implementation of the sorting itself, through the Insertion algorithm
+     * Receives the array reference as parameter and sorts it. Thus, no
+     * return is necessary
+     *
+     * This is an alternative implementation of the Insertion algorithm. It has
+     * an unorthodox swapping method, but it does work
+     *
+     * @param v vector to be sorted
+     */
+    public void sortAlternativeImpl(int[] v){
+        int x, j;
+        for(int i = 1; i < v.length; i++){
+            x = v[i];
+            j = i - 1;
+            while((j >= 0) && v[j] > x){
+                v[j + 1] = v[j];
+                j = j - 1;
+            }
+            v[j + 1] = x;
+        }
+    }
 }
