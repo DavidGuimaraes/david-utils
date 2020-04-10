c935d9949a6f6d7fa796cc490e847a0eb41f457d
David Guimaraes <david.ce@live.com>
2020-04-10 09:57:49 -0400

    04. Adding the implementation of the Insertion Sort algorithm in the class.

--- src/main/java/co/davidguimaraes/sort/InsertionSort.java
+++ src/main/java/co/davidguimaraes/sort/InsertionSort.java
@@ -1,4 +1,31 @@
 package main.java.co.davidguimaraes.sort;
 
+/**
+ * Author David Guimaraes, Computer Engineer
+ *
+ * Class that implements the Insertion Sort algorithm
+ */
+
 public class InsertionSort {
+
+    /**
+     * Implementation of the sorting itself, through the Insertion algorithm
+     * Receives the array reference as parameter and sorts it. Thus, no
+     * return is necessary
+     *
+     * @param v vector to be sorted
+     */
+    public void sort(int[] v){
+        int aux = 0;
+        for(int i = 1; i <= v.length - 1; i++){
+            for(int j = i; j > 0; j--){
+                if(v[j] < v[j-1]){
+                    //swap operation
+                    aux = v[j-1];
+                    v[j-1] = v[j];
+                    v[j] = aux;
+                }
+            }
+        }
+    }
 }
